e692f382e6a79bc365611ae9cfde6d8c7f09e44b
jannalmb00 <2322746@edu.vaniercollege.qc.ca>
2024-05-04 22:04:45 -0400

    Update style.css and update tour html

--- Delivery2/scriptTours.js
+++ Delivery2/scriptTours.js
@@ -9,11 +9,38 @@ toggleButton.addEventListener('click', () => {
   navBarLinks.classList.toggle('active');
 });
 
-function toggleDetails(moreDetails) {
-  var details = document.getElementById(moreDetails);
-  if (details.style.display === "none") {
-    details.style.display = "block"; // Show details
-  } else {
-    details.style.display = "none"; // Hide details
-  }
-}
+  //fetch data
+  fetch('data-tours.json')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    const tours = data.continents.reduce((acc, continent) => acc.concat(continent.tours), []);
+    const tourList = document.getElementById('tour-main');
+
+    // Create a document fragment to hold tour elements
+    const fragment = document.createDocumentFragment();
+
+    tours.forEach(tour => {
+      const tourElement = document.createElement('div');
+      tourElement.classList.add('tour');
+      tourElement.innerHTML = `
+        <img src="${tour.image}" alt="${tour.name}">
+        <h2>${tour.name}</h2>
+        <p>Location: ${tour.location}</p>
+        <p>Price: $${tour.price}</p>
+        <p>Duration: ${tour.duration_days} days</p>
+        <p>Description: ${tour.description}</p>
+      `;
+      fragment.appendChild(tourElement);
+    });
+
+    // Append the fragment to the tourList element
+    tourList.appendChild(fragment);
+  })
+  .catch(error => {
+    console.error('There was a problem with the fetch', error);
+  });
